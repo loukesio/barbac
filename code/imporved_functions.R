53e16bcd4c0d81026d32d82bbaa96b0617d4d8d3
loukesio <theodosiou@evolbio.mpg.de>
2023-06-08 18:20:17 +0200

    :cat: adding the pacakge

--- code/imporved_functions.R
+++ code/imporved_functions.R
@@ -8,6 +8,7 @@ library(here)
 library(dplyr)
 library(stringr)
 library(gt)
+library(gridExtra)
 
 #______________________________________________________________________
 # ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔  ╔═╗╔╗╔╔═╗       ┬─┐┌─┐┌─┐┌┬┐  ┌─┐┬┬  ┌─┐┌─┐
@@ -66,17 +67,24 @@ raw_counts <- function(file_path) {
 }
 test <- raw_counts(files[1])
 
-#_______________________________________________________________________________
-# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔  ╔═╗╔╗╔╔═╗ ╔═╗╔╗╔╔═╗       ┬─┐┌─┐┌─┐┌┬┐  ┌─┐┬┬  ┌─┐┌─┐
-# ╠╣ ║ ║║║║║   ║ ║║ ║║║║  ║ ║║║║║╣  ║ ║║║║║╣   ───  ├┬┘├┤ ├─┤ ││  ├┤ ││  ├┤ └─┐
-# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝  ╚═╝╝╚╝╚═╝o╚═╝╝╚╝╚═╝       ┴└─└─┘┴ ┴─┴┘  └  ┴┴─┘└─┘└─┘
-#_______________________________________________________________________________
+#_____________________________________________________
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔  ╔═╗╦ ╦╔╦╗   ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║  ╚═╗║ ║║║║───╚═╗ ║ ╠═╣ ║ ╚═╗
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝  ╚═╝╚═╝╩ ╩   ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+#_____________________________________________________
 
 library(dplyr)
 library(gt)
-library(gtExtras)
+library(gridExtra)
+library(ggplot2)
 
-table <- function(file, barcode_length) {
+test
+
+###########################
+# table 1 barcode - bins
+##########################
+
+sumstats <- function(file, barcode_length, fill_color = "#FF5349") {
   # Check if the 'barcode_length' parameter is a numeric vector of length 2
   if (!is.numeric(barcode_length) || length(barcode_length) != 2) {
     stop("Error: 'barcode_length' should be a numeric vector of length 2.")
@@ -87,7 +95,8 @@ table <- function(file, barcode_length) {
     stop("Error: 'file' should be a data frame or tibble.")
   }
   
-  file %>%
+  t1 <- 
+    file %>%
     mutate(
       bin = case_when(
         (length_barcode < barcode_length[1] & length_barcode > 0) ~ paste0("(", 0, ",", barcode_length[1], ")"),
@@ -97,52 +106,80 @@ table <- function(file, barcode_length) {
     ) %>%
     group_by(bin) %>%
     count() %>%
-    tableGrob()
-    # gt() %>%
-    # cols_label(n = "No of barcodes") %>%
-    # tab_source_note(
-    #   source_note = "Barbac: A versatile tool to quantify barcodes."
-    # ) %>%
-    # gt_theme_espn()
+    gridExtra::tableGrob()
+  
+  sumstats <- function(file, barcode_length, fill_color = "#FF5349") {
+  # Check if the 'barcode_length' parameter is a numeric vector of length 2
+  if (!is.numeric(barcode_length) || length(barcode_length) != 2) {
+    stop("Error: 'barcode_length' should be a numeric vector of length 2.")
+  }
+  
+  # Check if 'file' is a data frame or tibble
+  if (!is.data.frame(file) && !is.tbl(file)) {
+    stop("Error: 'file' should be a data frame or tibble.")
+  }
+  
+  t1 <- 
+    file %>%
+    mutate(
+      bin = case_when(
+        (length_barcode < barcode_length[1] & length_barcode > 0) ~ paste0("(", 0, ",", barcode_length[1], ")"),
+        (length_barcode >= barcode_length[1] & length_barcode <= barcode_length[2]) ~ paste0("[", barcode_length[1], ",", barcode_length[2], "]"),
+        TRUE ~ paste0("[", barcode_length[2] + 1, ",∞)")
+      )
+    ) %>%
+    group_by(bin) %>%
+    count() %>%
+    dplyr::rename(barcodes=n) %>% 
+    gridExtra::tableGrob(.,
+                       rows = NULL,
+                       theme = ttheme_default(
+                         core = list(bg_params = list(fill = "white"))
+                       ))
+  
+  p1 <- 
+    file %>% 
+    ggplot(., aes(x=length_barcode)) +
+    geom_histogram(fill = fill_color, color = NA, alpha = 0.8) +
+    labs(x="Barcode length (bp)", y="Number of barcodes", title="Barcode length distribution") + 
+    theme_bw() +
+    theme(plot.title = element_text(hjust=0.5))
+  
+  p2 <- 
+    file %>%   
+    ggplot(., aes(x=log(counts))) +
+    geom_histogram(fill = fill_color, color = NA, alpha = 0.8) +
+    labs(x="log(Barcode counts)", y="Number of barcodes", title="Barcode counts distribution") + 
+    theme_bw() +
+    theme(plot.title = element_text(hjust=0.5))
+  
+  shannon_entropy <- function(seq) {
+    n <- nchar(seq)
+    nucleotides <- unique(strsplit(seq, "")[[1]])
+    counts <- sapply(nucleotides, function(nuc) sum(strsplit(seq, "")[[1]] == nuc))
+    probabilities <- counts / n
+    entropy <- -sum(probabilities * log2(probabilities))
+    return(entropy)
+  }
+  
+  p3 <- 
+    file %>% 
+    mutate(entropy = sapply(barcode, shannon_entropy)) %>% 
+    ggplot() +
+    geom_histogram(aes(x=entropy), fill = fill_color) +
+    theme_bw()
+  
+  (p1 | p2) / (p3 | t1)
+  }
 }
 
-# Usage example
-test
+  
+
 file <- test  # Assuming 'data' is your input dataframe
 barcode_length <- c(22, 28)  # Specify the bin ranges here
 
-table(file, barcode_length)
+sumstats(file,barcode_length, fill_color = "blue")
 
-test %>% 
-  ggplot(., aes(x=length_barcode)) +
-  geom_histogram(fill="#FF5349", color=NA, alpha=0.8) +
-  labs(x="Barcode length (bp)", y="Number of barcodes", title="Barcode length distribution") + 
-  theme_bw() +
-  theme(plot.title = element_text(hjust=0.5))
-
-
-test %>%   
-ggplot(., aes(x=log(counts))) +
-geom_histogram(fill="#FF5349", color=NA, alpha=0.8) +
-  labs(x="log(Barcode counts)", y="Number of barcodes", title="Barcode counts distribution") + 
-  theme_bw() +
-  theme(plot.title = element_text(hjust=0.5))
-
-# Function to calculate Shannon entropy
-shannon_entropy <- function(seq) {
-  n <- nchar(seq)
-  nucleotides <- unique(strsplit(seq, "")[[1]])
-  counts <- sapply(nucleotides, function(nuc) sum(strsplit(seq, "")[[1]] == nuc))
-  probabilities <- counts / n
-  entropy <- -sum(probabilities * log2(probabilities))
-  return(entropy)
-}
-
-test %>% 
-  mutate(entropy = sapply(barcode, shannon_entropy)) %>% 
-  ggplot() +
-  geom_histogram(aes(x=entropy)) +
-  theme_b
 
 #__________________________________________________________________________ 
 # ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔  ╔╦╗╦ ╦╔═╗       ┌─┐┬  ┬ ┬┌─┐┌┬┐┌─┐┬─┐┬┌┐┌┌─┐
